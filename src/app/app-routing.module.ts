a24e31fad8c78c9fb31adab4620ab68393ba6c40
Alexandru Bej <alexbej07@gmail.com>
2020-10-12 22:29:19 +0300

    Clean for update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LayoutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full', component: LayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
